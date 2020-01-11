36a0734c521274863b2c1af662b5821dd9cf27ec
Nicola Erario <nicola.erario@gmail.com>
2020-01-11 19:27:25 +0100

    Auth App
    Added login, logout and signup functionalities
    using django default auth app;
    added login and signup pages and modded the home;
    added urls to users app and modded views. Added tests;
    updated project settings and urls

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin back end
     path("admin/", admin.site.urls),
+    # User auth and management
+    path("account/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("users.urls")),
     path("", include("pages.urls")),
 ]
