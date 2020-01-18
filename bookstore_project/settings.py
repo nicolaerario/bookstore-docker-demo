6cb532a195f3c2be037941549cced4719c483a45
Nicola Erario <nicola.erario@gmail.com>
2020-01-18 17:05:33 +0100

    Added environments variables
    env variables are in the .env file (that will be never put on vcs);
    an .env_example is provided as example..;
    docker compose file modified to read env and refer them to settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "*-eqc%-=tj_g-a8zl0_@*^ysjs9buzzffs%_$f#lch9o9c0905"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", 0))
 
 ALLOWED_HOSTS = []
 
